faa43fe48d2acb729219fb752bd94e96d38c8844
thiagohpr <thiagohpr@al.insper.edu.br>
2021-11-21 19:52:21 -0300

    primeira versão do backend, funções principais funcionando

---
+++ backend/lista/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Lista (models.Model):
+    title = models.CharField(max_length=200)
+    score = models.DecimalField(max_digits=3,decimal_places=1)
+    state = models.TextField()
